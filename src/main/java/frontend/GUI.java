e44a2bd21ff819ca14234fc3e8f874925259bd5e
Maria Jose Alarcon Osuna <mariajose.alarconosuna@ontariotechu.net>
2026-03-30 18:59:24 -0400

    new panel

--- src/main/java/frontend/GUI.java
+++ src/main/java/frontend/GUI.java
@@ -1,4 +1,14 @@
 package frontend;
 
-public class GUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame{
+    public  GUI(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,600);
+        setVisible(true);
+        setLayout(new FlowLayout());
+        JPanel panel = new JPanel();
+    }
 }
