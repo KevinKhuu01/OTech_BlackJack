3882ce3bde08898abe3c8ddf1f79972fee50f357
KevinKhuu01 <kevinkhuu01@gmail.com>
2026-03-31 15:00:19 -0400

    updated game logic and player classes: ace card handling

--- src/main/java/backend/GameLogic.java
+++ src/main/java/backend/GameLogic.java
@@ -1,12 +1,17 @@
 package backend;
 
+import java.io.IOException;
 import java.util.*;
 import static java.util.Collections.shuffle;
 
 public class GameLogic {
+
+    // Fields
     private static Map<String, Player> playerList;
     private static HashMap<Player, Integer> betList;
     private static Stack<Card> deck = new Stack<Card>();
+
+    // Constructor
     public GameLogic()
     {
         playerList = new HashMap<String, Player>();
@@ -14,34 +19,60 @@ public class GameLogic {
         addPlayer("dealer");
     }
 
+    // Methods
+    // Adds a new player to the current game
     public static void addPlayer(String name)
     {
-        Player p = new Player(name);
+        int random = (int)(Math.random() * 100000);
+        Player p = new Player(name, random);
         playerList.put(name, p);
     }
 
+    // Starts game: Ensures there are players in the game, create a new randomized deck, deals 2 cards to each player
     public static void startGame()
     {
-        newDeck();
-        for(Player p : playerList.values())
+        try
         {
-            for(int i = 0; i < 2; i++)
+            if(playerList.size() <= 0)
             {
-                hit(p);
+                throw new IOException("No players in game!");
+            }
+            newDeck();
+            for(Player p : playerList.values())
+            {
+                for(int i = 0; i < 2; i++)
+                {
+                    hit(p);
+                }
+            }
+            printGame();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    // Creates a new deck of cards
+    private static void newDeck()
+    {
+        for(Card.Face face : Card.Face.values())
+        {
+            for(Card.Suit suit : Card.Suit.values())
+            {
+                deck.push(new Card(face, suit));
             }
         }
-        printGame();
+        shuffleDeck();
     }
 
-    public static void printBalance()
+    // randomizes deck order
+    private static void shuffleDeck()
     {
-        for(Player p : playerList.values())
-        {
-            System.out.println(p.getName() + ": $" + p.getBalance());
-        }
-        System.out.println();
+        shuffle(deck);
     }
 
+    // takes card off top of deck and gives to player
     public static void hit(Player p)
     {
         if(p.getStatus() == Player.STATUS.LOST || p.getStatus() == Player.STATUS.WIN)
@@ -50,17 +81,10 @@ public class GameLogic {
         }
 
         p.getHand().add(deck.pop());
+        checkWin(p);
     }
 
-    public static void printGame()
-    {
-        for(Player p : playerList.values())
-        {
-            System.out.println(p.getName() + ": " + p.getHand() + " (" + p.getHandTotal() + ") " + " [" + checkWin(p) + "]");
-        }
-        System.out.println();
-    }
-
+    // check if a player has won
     public static String checkWin(Player p)
     {
         if(p.getHandTotal() == 21)
@@ -88,34 +112,37 @@ public class GameLogic {
         return p.getStatus().toString();
     }
 
-    private static void newDeck()
+    // prints all player balances (to be removed or refractored to return list)
+    public static void printBalance()
     {
-        for(Card.Face face : Card.Face.values())
+        for(Player p : playerList.values())
         {
-            for(Card.Suit suit : Card.Suit.values())
-            {
-                deck.push(new Card(face, suit));
-            }
+            System.out.println(p.getName() + ": $" + p.getBalance());
         }
-        shuffleDeck();
+        System.out.println();
     }
 
-    private static void shuffleDeck()
-    {
-        shuffle(deck);
-    }
-
+    // places a bet for a player
     private static void playerBet(Player p, int bet)
     {
         betList.put(p, bet);
+        p.withdrawBalance(bet);
     }
 
-    public static void main(String[] args)
+    // to be removed
+    public static void printGame()
     {
-        GameLogic game1 = new GameLogic();
-        addPlayer("Kevin");
-        addPlayer("bob");
-        startGame();
-        printBalance();
+        for(Player p : playerList.values())
+        {
+            System.out.println(p.getName() + ": " + p.getHand() + " (" + p.getHandTotal() + ") " + " [" + checkWin(p) + "]");
+        }
+        System.out.println();
     }
+//
+//    public static void main(String[] args)
+//    {
+//        GameLogic game1 = new GameLogic();
+//        startGame();
+//        printBalance();
+//    }
 }
